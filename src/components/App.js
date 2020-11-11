43fc7a37d991dfe4634fc8b157ffbf4a219f7521
Aquib <aquibshaikh02397@gmail.com>
2020-11-12 02:25:06 +0530

    code is changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,69 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px",
+    });
+  };
+
+  const ball = () => {
+    return <div className="ball" style={ballPosition}></div>;
+  };
+  const setx = (tx, ty) => {
+    setX(tx);
+    setY(ty);
+    setBallPosition({
+      left: tx + "px",
+      top: ty + "px",
+    });
+  };
+
+  useEffect(() => {
+    const onKeyDown = ({ key }) => {
+      let tempX = x;
+      let tempY = y;
+      let ALLOWED_KEYS = {
+        ArrowUp: () => (tempY = tempY - 5),
+        ArrowDown: () => (tempY = tempY + 5),
+        ArrowLeft: () => (tempX = tempX - 5),
+        ArrowRight: () => (tempX = tempX + 5),
+      };
+      if (renderBall && key in ALLOWED_KEYS) {
+        ALLOWED_KEYS[key]();
+      }
+      setx(tempX,tempY);
+      console.log(ballPosition);
+    };
+    
+    document.addEventListener("keydown", onKeyDown);
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+  }, [setx]);
+
+  const renderChoice = () => {
+    return renderBall ? (
+      ball()
+    ) : (
+      <button onClick={() => setRenderBall(true)} className="start">
+        Start
+      </button>
+    );
+  };
 
   return (
     <div className="playground">
